15dccc08e1ac7b8b90118aa03b48cf11002fd5a9
brian <brian@DESKTOP-PS5CJVP.wireless-1x.unc.edu>
2019-08-28 13:00:42 -0400

    fixed red lines

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -11,22 +11,26 @@ package a1;
 				// Your code follows here.
 				int loopnumber = scan.nextInt();
 				for (int i=0; i <= loopnumber; i++) {
-					System.out.println(scan.next() + ":");
+					System.out.println(scan.next());
 					System.out.println(scan.next() + ":");
 					loopnumber = scan.nextInt();
 					double totalamount = 0;
 					for (int j=0; j <= loopnumber; j++) {
 						int amount = scan.nextInt();
 						double price = scan.nextDouble();
-						double[] prices = new double[price];
+						double[] prices = new double[loopnumber]; //find how big the array is
 						
 						for (int k=0; k<=loopnumber; k++ ) {
 							prices[k] = amount * price;
+							if (k<loopnumber - 1) { 
 							amount = scan.nextInt();
 							price = scan.nextDouble();
+							}else{
+								k = 100;
+							}
 							
 							for (int l=0; l<prices.length; l++) {
-								double totalamount = totalamount + prices[l];
+								totalamount = totalamount + prices[l];
 								System.out.println(totalamount);
 								
 							}
